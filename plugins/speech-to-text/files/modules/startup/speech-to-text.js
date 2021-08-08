0bb55983e9ea6e2b5469537fe140932cd3119975
BurningTreeC <huber.simon@protonmail.com>
2021-08-08 18:11:28 +0200

    add license tiddler + add skeleton for startup script

---
+++ plugins/speech-to-text/files/modules/startup/speech-to-text.js
@@ -0,0 +1,25 @@
+/*\
+title: $:/plugins/flancast90/speech-to-text/modules/startup/speech-to-text.js
+type: application/javascript
+module-type: startup
+
+Speech to Text handling
+
+\*/
+(function(){
+
+/*jslint node: true, browser: true */
+/*global $tw: false */
+"use strict";
+
+// Export name and synchronous status
+exports.name = "speech-to-text";
+exports.platforms = ["browser"];
+exports.after = ["startup"];
+exports.synchronous = true;
+
+exports.startup = function() {
+
+};
+
+})();
