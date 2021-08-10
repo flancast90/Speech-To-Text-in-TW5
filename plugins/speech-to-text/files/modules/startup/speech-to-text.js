d961ff4a287481fc5a41e1fd93e08a50a4bfed4d
Finn Lancaster <flancast90@gmail.com>
2021-08-10 17:56:50 -0400

    command to change language GG explan. soon

--- plugins/speech-to-text/files/modules/startup/speech-to-text.js
+++ plugins/speech-to-text/files/modules/startup/speech-to-text.js
@@ -22,13 +22,19 @@ exports.startup = function() {
 	if(!window.SpeechRecognition && !window.webkitSpeechRecognition) {
 		return;
 	}
+    //default is English, but user can change via speech-commands later
+
 	var isRecording = false;
 	var fullTranscript = "";
 	var transcriptCounter = 0;
 	// required for API to initialise
 	var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+
+    // here we handle all speech web api specific settings. The names are
+    // a solid indicator of what they do.
 	var recognition = new SpeechRecognition();
 	recognition.continuous = true;
+    recognition.lang = 'en-US';
 
 	// This runs when the speech recognition service starts
 	recognition.onstart = function() {
@@ -83,6 +89,46 @@ exports.startup = function() {
 		var confidence = event.results[transcriptCounter][0].confidence;
 		transcriptCounter += 1;
 		fullTranscript = fullTranscript + transcript;
+
+        // WE CAN ADD COMMANDS IN THE BELOW IF STATEMENT, AS WELL AS WHAT THEY SHOULD DO.
+        
+        /*
+         * Command Support for versions >= 1.0.4. We will check if 
+         * transcript contains the word "command", and from there we can match the rest 
+         * to pre-defined command phrases. This will also ease the command-adding process.
+        */
+        var language_lowered = transcript.toLowerCase()
+        if (language_lowered.includes("command")) {
+            console.log("SPEECH-TO-TEXT PLUGIN: detected command word, awaiting further instructions.");
+            // this is the actual text of the command
+            var action = language_lowered.replace("command ", "");
+            if (action.includes("change language to ")){
+                console.log("SPEECH-TO-TEXT PLUGIN: change lang fired, awaiting to hear language name.");
+                var lang_selection = action.split("change language to ")[1];
+                console.log("SPEECH-TO-TEXT PLUGIN: attempting to change language to "+lang_selection);
+                // for switching language name to identifier such as en_US, we will 
+                // use arrays that share indexes as a sort of dictionary. For example,
+                // name[1] = English, and identifier[1]= en_US.
+
+                // NOTE: finnish is spelled as "finish" because the program always
+                // interprets it as such
+                const name = ["african", "indonesian", "malaysian", "catalonian", "dutch", "english", "spanish", "basque", "french", "croatian", "icelandic", "italian", "hungarian", "netherlandic", "norwegian", "polish", "portuguese", "romanian", "slavic", "finish", "swedish", "turkish", "bulgarian", "russian", "serbian", "korean", "chinese", "japanese", "latin"];
+                const identifier = ["af-ZA", "id-ID", "ms-MY", "ca-ES", "de-DE", "en-US", "es-ES", "eu-ES", "fr-FR", "hr-HR", "is-IS", "it-IT", "hu-HU", "nl-NL", "nb-NO", "pl-PL", "pt-PT", "ro-RO", "sk-SK", "fi-FI", "sv-SE", "tr-TR", "bg-BG", "ru-RU", "sr-RS", "ko-KR", "cmn-Hans-CN", "ja-JP", "la"];
+
+                var user_specified_name = name.indexOf(lang_selection);
+                // if error, tell the user.
+                if (user_specified_name == -1) {
+	                $tw.notifier.display("$:/plugins/flancast90/speech-to-text/ui/Notifications/error-finding-lang");
+                    recognition.stop()
+                // otherwise, the language code is the index of the spoken word
+                } else {
+                    recognition.lang = identifier[user_specified_name];
+                    console.log("SPEECH-TO-TEXT PLUGIN: language now is "+recognition.lang);
+                    recognition.stop()
+                }
+
+            }
+        }
 	};
 
 	$tw.wiki.addEventListener("change",function(changes) {
