7fcbda4958b5c5fc1416ae1448157c21549cc8f3
BurningTreeC <huber.simon@protonmail.com>
2021-08-11 08:38:25 +0200

    refactor: don't stop listening after language change

--- plugins/speech-to-text/files/modules/startup/speech-to-text.js
+++ plugins/speech-to-text/files/modules/startup/speech-to-text.js
@@ -22,19 +22,22 @@ exports.startup = function() {
 	if(!window.SpeechRecognition && !window.webkitSpeechRecognition) {
 		return;
 	}
-    //default is English, but user can change via speech-commands later
+	//default is English, but user can change via speech-commands later
 
 	var isRecording = false;
 	var fullTranscript = "";
 	var transcriptCounter = 0;
+	var isCommand = false;
+	var isLanguageChange = false;
 	// required for API to initialise
 	var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
 
-    // here we handle all speech web api specific settings. The names are
-    // a solid indicator of what they do.
+	// here we handle all speech web api specific settings. The names are
+	// a solid indicator of what they do.
 	var recognition = new SpeechRecognition();
 	recognition.continuous = true;
-    recognition.lang = 'en-US';
+	// WE SHOULDN'T SET THE LANGUAGE BY DEFAULT
+	//recognition.lang = 'en-US';
 
 	// This runs when the speech recognition service starts
 	recognition.onstart = function() {
@@ -42,93 +45,131 @@ exports.startup = function() {
 		// What is the best way to alert the user of 
 		// Mic recording? Put either dropdown, alert, etc.
 		// here for that functionality.
-		$tw.notifier.display("$:/plugins/flancast90/speech-to-text/ui/Notifications/recording-started");
+		if(!isLanguageChange) {
+			$tw.notifier.display("$:/plugins/flancast90/speech-to-text/ui/Notifications/recording-started");
 
-		// do something when recording starts
-		
-		$tw.wiki.setText("$:/state/speech-to-text/recording/ongoing","text",undefined,"yes");
+			// do something when recording starts
+			
+			$tw.wiki.setText("$:/state/speech-to-text/recording/ongoing","text",undefined,"yes");
+		} else {
+			isLanguageChange = false;
+		}
 	};
 
 	recognition.onspeechend = function() {
-		isRecording = false;
-		// when user is done speaking
-		recognition.stop();
+		if(!isLanguageChange) {
+			isRecording = false;
+			// when user is done speaking
+			recognition.stop();
 
-		// What is the best way to alert the user of 
-		// Mic-stopped recording? Use that method here.
-		//$tw.notifier.display("$:/plugins/flancast90/speech-to-text/ui/Notifications/recording-stopped");
-		
-		// WE WANT TO CHANGE BUTTON COLOUR BACK TO BLACK HERE
-		$tw.wiki.deleteTiddler("$:/state/speech-to-text/recording/ongoing");
-		$tw.wiki.deleteTiddler("$:/state/speech-to-text/recording");
+			// What is the best way to alert the user of 
+			// Mic-stopped recording? Use that method here.
+			//$tw.notifier.display("$:/plugins/flancast90/speech-to-text/ui/Notifications/recording-stopped");
+			
+			// WE WANT TO CHANGE BUTTON COLOUR BACK TO BLACK HERE
+			$tw.wiki.deleteTiddler("$:/state/speech-to-text/recording/ongoing");
+			$tw.wiki.deleteTiddler("$:/state/speech-to-text/recording");
+		}
 	}
 
 	recognition.onend = function() {
-		isRecording = false;
-		transcriptCounter = 0;
-		$tw.notifier.display("$:/plugins/flancast90/speech-to-text/ui/Notifications/recording-stopped");
+		if(!isLanguageChange) {
+			isRecording = false;
+			transcriptCounter = 0;
+			$tw.notifier.display("$:/plugins/flancast90/speech-to-text/ui/Notifications/recording-stopped");
 
-		// WE WANT TO DISPLAY THE OUTPUT (saved in var transcript) IN TW5
-		var creationFields = $tw.wiki.getCreationFields();
-		var modificationFields = $tw.wiki.getModificationFields();
-		var title = $tw.wiki.generateNewTitle("New Transcript");
-		if(fullTranscript !== "") {
-			$tw.wiki.addTiddler(new $tw.Tiddler(creationFields,modificationFields,{title: title,text: fullTranscript}));
-			$tw.rootWidget.invokeActionString('<$navigator story="$:/StoryList" history="$:/HistoryList"><$action-sendmessage $message="tm-edit-tiddler" $param="' + title + '"/></$navigator>');
+			// WE WANT TO DISPLAY THE OUTPUT (saved in var transcript) IN TW5
+			var creationFields = $tw.wiki.getCreationFields();
+			var modificationFields = $tw.wiki.getModificationFields();
+			var title = $tw.wiki.generateNewTitle("New Transcript");
+			if(fullTranscript !== "") {
+				$tw.wiki.addTiddler(new $tw.Tiddler(creationFields,modificationFields,{title: title,text: fullTranscript}));
+				$tw.rootWidget.invokeActionString('<$navigator story="$:/StoryList" history="$:/HistoryList"><$action-sendmessage $message="tm-edit-tiddler" $param="' + title + '"/></$navigator>');
+			}
+			
+			fullTranscript = "";
+			
+			// WE WANT TO CHANGE BUTTON COLOUR BACK TO BLACK HERE
+			$tw.wiki.deleteTiddler("$:/state/speech-to-text/recording/ongoing");
+			$tw.wiki.deleteTiddler("$:/state/speech-to-text/recording");
+			$tw.wiki.deleteTiddler("$:/state/speech-to-text/transcript");
+		} else if(isLanguageChange) {
+			transcriptCounter = 0;
+			recognition.start();
+			$tw.notifier.display("$:/plugins/flancast90/speech-to-text/ui/Notifications/language-switch",{variables: {language: recognition.lang}})
 		}
-		
-		fullTranscript = "";
-		
-		// WE WANT TO CHANGE BUTTON COLOUR BACK TO BLACK HERE
-		$tw.wiki.deleteTiddler("$:/state/speech-to-text/recording/ongoing");
-		$tw.wiki.deleteTiddler("$:/state/speech-to-text/recording");
 	}
 
 	recognition.onresult = function(event) {
 		var transcript = event.results[transcriptCounter][0].transcript;
 		var confidence = event.results[transcriptCounter][0].confidence;
+
+		if(transcriptCounter === 0) {
+			transcript = transcript.charAt(0).toUpperCase() + transcript.slice(1);
+		}
+
+		var transcriptChunk;
+		// WE CAN ADD COMMANDS IN THE BELOW IF STATEMENT, AS WELL AS WHAT THEY SHOULD DO.
+		
+		/*
+		 * Command Support for versions >= 1.0.4. We will check if 
+		 * transcript contains the word "command", and from there we can match the rest 
+		 * to pre-defined command phrases. This will also ease the command-adding process.
+		*/
+		//var language_lowered = transcript.toLowerCase();
+		if (transcript.includes("command") || transcript.includes("Command")) {
+
+			isCommand = true;
+			transcriptChunk = transcript;
+			console.log("SPEECH-TO-TEXT PLUGIN: detected command word, awaiting further instructions.");
+			// this is the actual text of the command
+			var action;
+			if(transcript.includes("command")) {
+				action = transcriptChunk.replace("command ", "");
+				transcriptChunk = transcriptChunk.replace("command ", "");
+			} else if(transcript.includes("Command")) {
+				action = transcriptChunk.replace("Command", "");
+				transcriptChunk = transcriptChunk.replace("Command ", "");
+			}
+			if (action.includes("change language to ")) {
+				console.log("SPEECH-TO-TEXT PLUGIN: change lang fired, awaiting to hear language name.");
+				var lang_selection = action.split("change language to ")[1];
+				console.log("SPEECH-TO-TEXT PLUGIN: attempting to change language to " + lang_selection);
+				// for switching language name to identifier such as en_US, we will 
+				// use arrays that share indexes as a sort of dictionary. For example,
+				// name[1] = English, and identifier[1]= en_US.
+
+				// NOTE: finnish is spelled as "finish" because the program always
+				// interprets it as such
+				var name = ["african", "indonesian", "malaysian", "catalonian", "german", "english", "spanish", "basque", "french", "croatian", "icelandic", "italian", "hungarian", "netherlandic", "norwegian", "polish", "portuguese", "romanian", "slavic", "finish", "swedish", "turkish", "bulgarian", "russian", "serbian", "korean", "chinese", "japanese", "latin"];
+				var identifier = ["af-ZA", "id-ID", "ms-MY", "ca-ES", "de-DE", "en-US", "es-ES", "eu-ES", "fr-FR", "hr-HR", "is-IS", "it-IT", "hu-HU", "nl-NL", "nb-NO", "pl-PL", "pt-PT", "ro-RO", "sk-SK", "fi-FI", "sv-SE", "tr-TR", "bg-BG", "ru-RU", "sr-RS", "ko-KR", "cmn-Hans-CN", "ja-JP", "la"];
+
+				var user_specified_name = name.indexOf(lang_selection.toLowerCase());
+				transcriptChunk = transcriptChunk.replace("change language to " + lang_selection, "");
+				// if error, tell the user.
+				if (user_specified_name === -1) {
+					$tw.notifier.display("$:/plugins/flancast90/speech-to-text/ui/Notifications/error-finding-lang",{variables:{language:lang_selection}});
+					//recognition.stop();
+				// otherwise, the language code is the index of the spoken word
+				} else {
+					recognition.lang = identifier[user_specified_name];
+					console.log("SPEECH-TO-TEXT PLUGIN: language now is "+recognition.lang);
+					isLanguageChange = true;
+					recognition.stop();
+				}
+
+			}
+		}
+
 		transcriptCounter += 1;
-		fullTranscript = fullTranscript + transcript;
-
-        // WE CAN ADD COMMANDS IN THE BELOW IF STATEMENT, AS WELL AS WHAT THEY SHOULD DO.
-        
-        /*
-         * Command Support for versions >= 1.0.4. We will check if 
-         * transcript contains the word "command", and from there we can match the rest 
-         * to pre-defined command phrases. This will also ease the command-adding process.
-        */
-        var language_lowered = transcript.toLowerCase()
-        if (language_lowered.includes("command")) {
-            console.log("SPEECH-TO-TEXT PLUGIN: detected command word, awaiting further instructions.");
-            // this is the actual text of the command
-            var action = language_lowered.replace("command ", "");
-            if (action.includes("change language to ")){
-                console.log("SPEECH-TO-TEXT PLUGIN: change lang fired, awaiting to hear language name.");
-                var lang_selection = action.split("change language to ")[1];
-                console.log("SPEECH-TO-TEXT PLUGIN: attempting to change language to "+lang_selection);
-                // for switching language name to identifier such as en_US, we will 
-                // use arrays that share indexes as a sort of dictionary. For example,
-                // name[1] = English, and identifier[1]= en_US.
-
-                // NOTE: finnish is spelled as "finish" because the program always
-                // interprets it as such
-                const name = ["african", "indonesian", "malaysian", "catalonian", "dutch", "english", "spanish", "basque", "french", "croatian", "icelandic", "italian", "hungarian", "netherlandic", "norwegian", "polish", "portuguese", "romanian", "slavic", "finish", "swedish", "turkish", "bulgarian", "russian", "serbian", "korean", "chinese", "japanese", "latin"];
-                const identifier = ["af-ZA", "id-ID", "ms-MY", "ca-ES", "de-DE", "en-US", "es-ES", "eu-ES", "fr-FR", "hr-HR", "is-IS", "it-IT", "hu-HU", "nl-NL", "nb-NO", "pl-PL", "pt-PT", "ro-RO", "sk-SK", "fi-FI", "sv-SE", "tr-TR", "bg-BG", "ru-RU", "sr-RS", "ko-KR", "cmn-Hans-CN", "ja-JP", "la"];
-
-                var user_specified_name = name.indexOf(lang_selection);
-                // if error, tell the user.
-                if (user_specified_name == -1) {
-	                $tw.notifier.display("$:/plugins/flancast90/speech-to-text/ui/Notifications/error-finding-lang");
-                    recognition.stop()
-                // otherwise, the language code is the index of the spoken word
-                } else {
-                    recognition.lang = identifier[user_specified_name];
-                    console.log("SPEECH-TO-TEXT PLUGIN: language now is "+recognition.lang);
-                    recognition.stop()
-                }
-
-            }
-        }
+		if(!isCommand) {
+			fullTranscript = fullTranscript + transcript;
+			$tw.wiki.setText("$:/state/speech-to-text/transcript","text",undefined,fullTranscript);
+		} else {
+			fullTranscript = fullTranscript + transcriptChunk;
+			$tw.wiki.setText("$:/state/speech-to-text/transcript","text",undefined,fullTranscript);
+		}
+		isCommand = false;
 	};
 
 	$tw.wiki.addEventListener("change",function(changes) {
